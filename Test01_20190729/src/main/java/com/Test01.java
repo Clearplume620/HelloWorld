7bdafe53402ad0961bb87911b2fe05541b9c6c97
wangman <105133951@qq.com>
2019-07-29 08:39:17 +0800

    第N次上传！

---
+++ Test01_20190729/src/main/java/com/Test01.java
@@ -0,0 +1,18 @@
+package com;
+
+/**
+ * @Author:Lenvo
+ * @Description:
+ * @Date: 2019/7/29 8:24
+ */
+public class Test01 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("老哥你好水");
+        System.out.println("老哥你好帅");
+        System.out.println("老哥你好丑");
+    }
+
+}
